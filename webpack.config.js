3d245d2ad5808a399b2a9f5271190e497b4ad284
Nick <nicholasrcarlson@gmail.com>
2018-04-09 16:00:26 -0500

    completed lazy loading

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ var webpack = require('webpack');
 var loaders = require('babel-loader');
 
 module.exports = {
-    entry: "./app/assets/scripts/App.js",
+    entry: {
+        App: "./app/assets/scripts/App.js",
+        Vendor: "./app/assets/scripts/Vendor.js"
+    },
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
-        filename: "App.js"
+        filename: "[name].js"
     },
     
     module: {
